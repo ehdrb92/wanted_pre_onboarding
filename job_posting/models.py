f96c145ca6fd89209ebaeaf360acc0786220b794
ehdrb92 <kcj920407@gmail.com>
2022-10-15 15:46:50 +0900

    feat : database modeling
    * user/models.py
    
    1. User: 구직자 정보 테이블
    2. Company: 채용기업 정보 테이블
    3. Resume: 지원서 정보 테이블
    
    * job_posting/models.py
    
    1. Post: 채용정보 테이블

--- job_posting/models.py
+++ job_posting/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from user.models import Company
+
+class Post(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    country = models.CharField(max_length=10)
+    region = models.CharField(max_length=10)
+    position = models.CharField(max_length=20)
+    compensation = models.PositiveIntegerField()
+    skill = models.CharField(max_length=100)
+    context = models.TextField()
+
+    class Meta:
+        db_table = 'post'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from job_posting.models import Post
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'user'
+
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'company'
+
+class Resume(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'resume'
